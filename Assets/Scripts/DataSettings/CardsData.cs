bc23a895b05036673188ee5820263fa94a848d5f
lenon3579 <lenon3579@gmail.com>
2020-07-17 16:19:48 +0100

    last fixes

--- Assets/Scripts/DataSettings/CardsData.cs
+++ Assets/Scripts/DataSettings/CardsData.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System;
+using UnityEngine.Networking;
 
 public class CardsData : MonoBehaviour
 {
@@ -12,21 +14,37 @@ public class CardsData : MonoBehaviour
     public void LoadGameData() {
         string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
 
-        if (File.Exists(filePath)) {
-            string dataAsJson = File.ReadAllText(filePath);
+        string newPath = Path.Combine(Application.persistentDataPath, fileName);
 
-            //Debug.Log(dataAsJson);
+        DoWWW_Android(fileName, filePath, newPath);
+
+        if (File.Exists(newPath)) {
+            string dataAsJson = File.ReadAllText(newPath);
 
             CardsRoot loadedData = JsonUtility.FromJson<CardsRoot>(dataAsJson);
 
             cards = loadedData.cardsRoot;
 
-            Debug.Log("Streaming Cards: " + loadedData.cardsRoot.Length);
-
-            DebugCards();
+            Debug.Log("Streaming Armors: " + loadedData.cardsRoot.Length);
         }
         else {
             Debug.LogError("Inventory Data file missing!");
+        }
+
+    }
+
+    static void DoWWW_Android(string name, string origPath, string newPath) {
+        using (UnityWebRequest reader = UnityWebRequest.Get(origPath)) {
+            reader.SendWebRequest();
+            while (!reader.isDone) {
+            }
+            if (!reader.isHttpError && !reader.isNetworkError) {
+                File.WriteAllBytes(newPath, reader.downloadHandler.data);
+            }
+            else {
+                Debug.LogError("Database " + name + " not found at " + origPath);
+                Debug.LogError("Error: " + reader.error);
+            }
         }
     }
 
